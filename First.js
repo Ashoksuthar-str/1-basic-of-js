c850b592e0062ce4017c691fad84da725761ab3f
All T C <76059900+Ashoksuthar-str@users.noreply.github.com>
2025-04-09 20:20:29 +0530

    Create First.js

---
+++ First.js
@@ -0,0 +1,18 @@
+const accountId = 123456
+let accountEmail = "sutharashok191@gmail.com"
+var accountPassword = "MyP4ss"
+accountCity = "Sirohi"
+
+//accountId = 654321 // not allowed
+
+/*
+multiline 
+comment
+*/
+
+
+accountEmail = "notValid@email.com"
+accountPassword = "Y0urP4ss"
+accountCity = "Visnagar"
+
+console.log(accountId,accountEmail,accountPassword,accountCity)
