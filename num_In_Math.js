8981afd9b0234b726f6a3d1e058de43ad04c8268
Ashoksuthar-str <sutharashok191@gmail.com.com>
2025-04-10 11:01:59 +0530

    Math and num

---
+++ num_In_Math.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(400)
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(typeof balance.toString().length);
+
+// console.log(balance.toFixed(3));
+
+// const newNum = 23.973
+// console.log(newNum.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+/* -------------------  Maths  ------------------- */
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(3.9));
+// console.log(Math.ceil(3.3));
+// console.log(Math.floor(7.9));
+// console.log(Math.min(2,7,4,7,0,1));
+// console.log(Math.max(3,6,9,32,2));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*100) + 1);
+
+const min = 4
+const max = 20
+console.log(Math.floor((Math.random()*(max-min+1))+min));
